744ebfcf19d8d01b072fd2c468b52af3aa117c3b
talset <talset55@gmail.com>
2021-03-04 10:16:00 +0100

    demo: fix sed path

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ set -x
 export COMMIT=${COMMIT:-origin/master}
 
 set +e
-sed "s/@PROJECT@/$PROJECT/; s/@ENV@/$ENV/; s!@COMMIT@!$COMMIT!" -i /usr/share/nginx/*.php /usr/share/nginx/*.html
+sed "s/@PROJECT@/$PROJECT/; s/@ENV@/$ENV/; s!@COMMIT@!$COMMIT!" -i /usr/share/nginx/html/*.php /usr/share/nginx/html/*.html
 set -e
 
 exec nginx -g "daemon off;"
